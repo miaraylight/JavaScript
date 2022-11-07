cdf52a62e301f189d3a38c9350c01ebf4d5075bc
miaraylight <miaraylight@gmail.com>
2022-11-07 18:36:54 +0100

    hw update

--- homeworks/24oct22hw/scripts.js
+++ homeworks/24oct22hw/scripts.js
@@ -1,16 +1,16 @@
-//First level: 1. Фильтрация по диапазону
+// First level: 1. Фильтрация по диапазону
 
-//Напишите функцию filterRange(arr, a, b), которая принимает массив arr,
-//ищет элементы со значениями больше или равными a и меньше или равными b и возвращает результат в виде массива.
+// Напишите функцию filterRange(arr, a, b), которая принимает массив arr,
+// ищет элементы со значениями больше или равными a и меньше или равными b и возвращает результат в виде массива.
 
-//Функция должна возвращать новый массив и не изменять исходный.
+// Функция должна возвращать новый массив и не изменять исходный.
 
-//Например:
-//let arr = [5, 3, 8, 1];
-//let filtered = filterRange(arr, 1, 4);
+// Например:
+// let arr = [5, 3, 8, 1];
+// let filtered = filterRange(arr, 1, 4);
 
-//console.log( filtered ); // 3,1 (совпадающие значения)
-//console.log( arr ); // 5,3,8,1 (без изменений)
+// console.log( filtered ); // 3,1 (совпадающие значения)
+// console.log( arr ); // 5,3,8,1 (без изменений)
 
 // let filterRange = (arr, a, b) => {
 //     let result = [];
@@ -18,12 +18,9 @@
 //         if (arr[i] >= a && arr[i] <= b) {
 //             result.push(arr[i]);
 //             console.log(arr[i]);
-//             return result;
 //         }
-
-        
 //     }
-//     return arr;
+//     return result;
 // }
 // let arr = [5, 3, 8, 1];
 // let filtered = filterRange(arr, 1, 4);
@@ -38,29 +35,29 @@
 
 
 
-//Напишите функцию filterRangeInPlace(arr, a, b), 
-//которая принимает массив arr и удаляет из него все значения кроме тех, 
-//которые находятся между a и b. То есть, проверка имеет вид a ≤ arr[i] ≤ b.
+// Напишите функцию filterRangeInPlace(arr, a, b), 
+// которая принимает массив arr и удаляет из него все значения кроме тех, 
+// которые находятся между a и b. То есть, проверка имеет вид a ≤ arr[i] ≤ b.
 
 
-//Функция должна изменять принимаемый массив и ничего не возвращать.
+// Функция должна изменять принимаемый массив и ничего не возвращать.
 
-//Например:
-//let arr = [5, 3, 8, 1];
-//filterRangeInPlace(arr, 1, 4); // удалены числа вне диапазона 1..4
-//console.log( arr ); // [3, 1]
+// Например:
+// let arr = [5, 3, 8, 1];
+// filterRangeInPlace(arr, 1, 4); // удалены числа вне диапазона 1..4
+// console.log( arr ); // [3, 1]
 
-// function filterRangeInPlace(arr, a, b) {
-//     let iterArray = [];
+function filterRangeInPlace(arr, a, b) {
 
-//     arr.forEach(item => {
-//         if (item >= a && item <= b) {
-//             iterArray.push(item)
-//         }
-//     }
-//     )
-//     arr = iterArray;
-// }
+    for (let i = 0; i < arr.length; i++) {
+       if (arr[i] < a || arr[i] > b) {
+            arr.splice(i, 1);
+            i--;
+        } 
+        
+    }
+    
+}
 
 
 
